1267c71b94cc935c3787e903a1a7c66f3641004d
ahmed <hetetlahma@gmail.com>
2023-06-23 03:08:24 +0300

    start

--- client/src/components/Benefits.jsx
+++ client/src/components/Benefits.jsx
@@ -4,4 +4,5 @@ const Benefits = () => {
   return <div>Benefits</div>;
 };
 
+
 export default Benefits;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,4 +4,6 @@ const Header = () => {
   return <div>Header</div>;
 };
 
+
+
 export default Header;
